49d2f8fcbb8b94c62862222583ad51ae9ae77107
Hideaki-Ryu <cenarahmant@gmail.com>
2023-07-20 15:48:32 +0700

    added API: product PUT by id, DEL by id, & user PUT by id

--- routes/product.js
+++ routes/product.js
@@ -1,48 +1,90 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var Product = require('../models/productModels');
+var Product = require("../models/productModels");
 
 /* GET List Product. */
-router.get('/', async function(req, res, next) {
+router.get("/", async function (req, res, next) {
   try {
     const product = await Product.find({});
     res.status(200).json({
-      message: 'List of all products',
-      products: product
-    })
+      message: "List of all products",
+      products: product,
+    });
   } catch (err) {
     console.log(err.message);
-    res.status(500).json({message: err.message});
+    res.status(500).json({ message: err.message });
   }
 });
 
 /* GET List Product by ID. */
-router.get('/:id', async function(req, res, next) {
+router.get("/:id", async function (req, res, next) {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const product = await Product.findById(id);
     res.status(200).json({
-      message: 'The product has been found',
-      products: product
-    })
+      message: "The product has been found",
+      products: product,
+    });
   } catch (err) {
     console.log(err.message);
-    res.status(500).json({message: err.message});
+    res.status(500).json({ message: err.message });
   }
 });
 
 /* POST Create Product. */
-router.post('/', async function (req, res, next) {
+router.post("/", async function (req, res, next) {
   try {
     const product = await Product.create(req.body);
-    res.status(200).json({ 
-      message: 'Successfully created a product',
-      product: product 
+    res.status(200).json({
+      message: "Successfully created a product",
+      product: product,
     });
   } catch (err) {
     console.log(err.message);
-    res.status(500).json({message: err.message});
+    res.status(500).json({ message: err.message });
   }
 });
+
+/* PUT Update Product. */
+router.put('/:id', async function(req, res, next) {
+  try {
+    const { id } = req.params;
+    const updates = req.body; 
+
+    const updatedProduct = await Product.findByIdAndUpdate(id, updates, { new: true });
+    res.status(200).json({
+      message: 'The product has been updated',
+      product: updatedProduct
+    });
+  } catch (err) {
+    if (err.name === 'CastError' && err.kind === 'ObjectId') {
+      console.log("Invalid product ID format");
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    console.log(err.message);
+    res.status(500).json({ message: err.message });
+  }
+});
+
+
+/* DEL List Product by ID. */
+router.delete('/:id', async function(req, res, next) {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    res.status(200).json({
+      message: 'The product has been deleted',
+      product: product
+    });
+  } catch (err) {
+    if (err.name === 'CastError' && err.kind === 'ObjectId') {
+      console.log("Invalid product ID format");
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    console.log(err.message);
+    res.status(500).json({ message: err.message });
+  }
+});
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -17,20 +17,19 @@ router.get("/", async function (req, res, next) {
 });
 
 /* GET List Product by ID. */
-router.get('/:id', async function(req, res, next) {
+router.get("/:id", async function (req, res, next) {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const user = await User.findById(id);
     res.status(200).json({
-      message: 'The user has been found',
-      user: user
-    })
+      message: "The user has been found",
+      user: user,
+    });
   } catch (err) {
     console.log(err.message);
-    res.status(500).json({message: err.message});
+    res.status(500).json({ message: err.message });
   }
 });
-
 
 /* POST Create User. */
 router.post("/", async function (req, res, next) {
@@ -41,6 +40,29 @@ router.post("/", async function (req, res, next) {
       user: user,
     });
   } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/* PUT Update User. */
+router.put("/:id", async function (req, res, next) {
+  try {
+    const { id } = req.params;
+    const updates = req.body;
+
+    const updatedUser = await User.findByIdAndUpdate(id, updates, {
+      new: true,
+    });
+    res.status(200).json({
+      message: "The user has been updated",
+      user: updatedUser,
+    });
+  } catch (err) {
+    if (err.name === "CastError" && err.kind === "ObjectId") {
+      console.log("Invalid product ID format");
+      return res.status(404).json({ message: "User not found" });
+    }
     console.log(err.message);
     res.status(500).json({ message: err.message });
   }
