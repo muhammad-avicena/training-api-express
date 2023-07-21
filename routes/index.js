9f1fa45bacb81e8158dc1d68e7207495be4bc2e4
Hideaki-Ryu <cenarahmant@gmail.com>
2023-07-22 01:44:05 +0700

    modified API: api/user & api/product, modified styling view index

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ var router = express.Router();
 // All routes
 const routes = [
   { path: '/', method: 'GET', description: 'Homepage' },
-  { path: '/api/users', method: 'GET', description: 'Get all users' },
-  { path: '/api/users', method: 'POST', description: 'Create a user' },
-  { path: '/api/users/:id', method: 'GET', description: 'Get a user by ID' },
-  { path: '/api/users/:id', method: 'PUT', description: 'Update a user by ID' },
+  { path: '/api/user', method: 'GET', description: 'Get all users' },
+  { path: '/api/user', method: 'POST', description: 'Create a user' },
+  { path: '/api/user/:id', method: 'GET', description: 'Get a user by ID' },
+  { path: '/api/user/:id', method: 'PUT', description: 'Update a user by ID' },
   { path: '/api/product', method: 'GET', description: 'Get all products' },
   { path: '/api/product', method: 'POST', description: 'Create a product' },
   { path: '/api/product/:id', method: 'GET', description: 'Get a product by ID' },
