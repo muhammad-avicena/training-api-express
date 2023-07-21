7fffbe116ba11f6cf046fbb16f449ed879ba2852
Hideaki-Ryu <cenarahmant@gmail.com>
2023-07-21 23:30:54 +0700

    modified routes index.js

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,11 @@ const routes = [
   { path: '/api/users', method: 'POST', description: 'Create a user' },
   { path: '/api/users/:id', method: 'GET', description: 'Get a user by ID' },
   { path: '/api/users/:id', method: 'PUT', description: 'Update a user by ID' },
-  { path: '/api/products', method: 'GET', description: 'Get all products' },
-  { path: '/api/products', method: 'POST', description: 'Create a product' },
-  { path: '/api/products/:id', method: 'GET', description: 'Get a product by ID' },
-  { path: '/api/products/:id', method: 'PUT', description: 'Update a product by ID' },
-  { path: '/api/products/:id', method: 'DEL', description: 'Delete a product by ID' },
+  { path: '/api/product', method: 'GET', description: 'Get all products' },
+  { path: '/api/product', method: 'POST', description: 'Create a product' },
+  { path: '/api/product/:id', method: 'GET', description: 'Get a product by ID' },
+  { path: '/api/product/:id', method: 'PUT', description: 'Update a product by ID' },
+  { path: '/api/product/:id', method: 'DEL', description: 'Delete a product by ID' },
 ];
 
 // GET home page.
